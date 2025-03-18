28e19860c3087131db5a6cd6a42191c6d5309b42
aglover02 <ga63717@mail.harpercollege.edu>
2025-03-17 21:29:27 -0500

    idk

--- Assignment9/lesson09.php
+++ Assignment9/lesson09.php
@@ -9,7 +9,7 @@ $db = new \PDO(
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pizza Order System</title>
     <script>
